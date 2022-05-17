33ba85f9bd0386cfbcdb7e29e788510e59dfab81
Attila Sebők <cowby@mailbox.unideb.hu>
2022-05-17 23:52:54 +0200

    háttérszín és egyéb beállítások

--- src/main/java/hu/unideb/inf/fxmlNewRel.java
+++ src/main/java/hu/unideb/inf/fxmlNewRel.java
@@ -1,12 +1,23 @@
 package hu.unideb.inf;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 public class fxmlNewRel {
 
     @FXML
     private ChoiceBox testalkat;
+
+    @FXML
+    private TextField neName;
+
+    @FXML
+    private TextField telephoneNumber;
+
+    @FXML
+    private CheckBox active;
 
     @FXML
     void pushButtonSave(){
